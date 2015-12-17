980ed04fb85951612fd8f2964a7c0371455cce68
pcanilho <papmc@iscte-iul.pt>
2015-12-17 16:40:25 +0100

    Version 1.0

---
+++ scripts/bash/test.sh
@@ -0,0 +1,10 @@
+if [[ $# -eq 0 ]] ; then
+    echo 'No arguments have been supplied.';
+    exit 1;
+else 
+ 
+ echo "$1 is a Bacalhau.";
+
+
+  
+fi
---
+++ workspace/eclipse-workspace/C++Test/Debug/src/C++Test.d
@@ -0,0 +1,8 @@
+src/C++Test.d: ../src/C++Test.cpp ../src/lib/Examples.hpp \
+ ../src/lib/includes.h ../src/lib/includes.h
+
+../src/lib/Examples.hpp:
+
+../src/lib/includes.h:
+
+../src/lib/includes.h:
